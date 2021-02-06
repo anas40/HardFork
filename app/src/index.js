0e9e6ccc85335441432ca9650dbf02112bc50052
Arifa99 <arifanafees99@gmail.com>
2021-02-06 13:00:53 +0530

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil'
 import App from './App';
+import Loader from './components/loader'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<Loader />} >
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
