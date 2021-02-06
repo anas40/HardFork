7f5d8b61f9f2feecb2eb0d93596045a803e4adb1
skahmed <skahmed@myamu.ac.in>
2021-02-06 11:12:27 +0530

    Added boiler-plate code for QR code generation

---
+++ app/src/pages/qrcode.js
@@ -0,0 +1,17 @@
+import React from "react";
+import QRCode from "react-qr-code";
+
+// css
+import "../static/css/info.scss";
+
+export default function ProductQrCode() {
+  const productId = window.location.pathname.split("/")[2];
+  return (
+    <div className="center-elements">
+      <h2 className="center-heading message-heading">
+        PRODUCT ADDED SUCCESSFULLY !
+      </h2>
+      <h2 className="message-heading">PRODUCT QR CODE</h2>
+    </div>
+  );
+}
