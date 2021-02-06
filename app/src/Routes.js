7cd7129933b4a0221378fa2c68105cde53f5e8de
gauharayub <gauharayub14@gmail.com>
2021-02-06 10:43:38 +0530

    added add product route

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -9,6 +9,10 @@ import { login as ll } from './store/atoms'
 import { useRecoilValue } from 'recoil'
 const Home = lazy(() => import(/* webpackChunkName: "HOME" */ './pages/home'))
 const Login = lazy(() => import(/*webpackChunkName: "LOGIN" */ './pages/login'))
+const Info = lazy(() => import(/*webpackChunkName: "INFO" */ './pages/info'))
+const BuyProduct = lazy(() => import(/*webpackChunkName: "BUYPRODUCT" */ './pages/buyProduct'))
+const AddProduct = lazy(() => import(/*webpackChunkName: "ADD" */ './pages/add'))
+const ProductInfo = lazy(() => import('./pages/productInfo'))
 
 function Routes() {
 
@@ -19,6 +23,18 @@ function Routes() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path='/login' component={Login} />
+          <PrivateRoute path="/product/:id" >
+            <Info />
+          </PrivateRoute>
+          <PrivateRoute path='/buy' exact >
+            <BuyProduct />
+          </PrivateRoute>
+          <PrivateRoute path='/productinfo/:id' >
+            <ProductInfo />
+          </PrivateRoute>
+          <PrivateRoute path='/add' >
+            <AddProduct />
+          </PrivateRoute>
         </Switch>
         <Route component={Footer} />
       </Router>
