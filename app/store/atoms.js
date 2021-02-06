2ed40b808f4168711acda4b6bb67f5e0f5603a13
gauharayub <gauharayub14@gmail.com>
2021-02-06 10:18:50 +0530

    added atoms.js

---
+++ app/store/atoms.js
@@ -0,0 +1,13 @@
+import { atom } from "recoil"
+
+const login = atom({
+    key: 'login',
+    default: false,
+})
+
+const popups = atom({
+    key: 'popups',
+    default: ''
+})
+
+export { login, popups }
