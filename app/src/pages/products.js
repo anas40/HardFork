bee350a55be6b966e4fd8e9c12a5c2040e4c1307
skahmed <skahmed@myamu.ac.in>
2021-02-06 13:07:55 +0530

    Added productList map in products.js

--- app/src/pages/products.js
+++ app/src/pages/products.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
+import Loader from "../components/loader";
 import { popups } from "../store/atoms";
 import provider from "../store/web3Provider";
-import Loader from "../components/loader";
+// css-imports
+import "../static/css/products.css";
 
 export default function Products() {
   const setPopup = useSetRecoilState(popups);
+  const history = useHistory();
+  const [loading, setLoading] = useState(true);
+  const [productList, setProducts] = useState("");
+  const [failMessage, setFailMessage] = useState("");
 
   useEffect(async () => {
     try {
@@ -28,11 +35,34 @@ export default function Products() {
     // setProducts([[4356, "this is medicine", "Aspirin"], [5556, "this is branded shoe", "Adidas Neo"]])
   }, []);
 
+  const productInfo = (productId) => {
+    history.push(`/productinfo/${productId}`);
+  };
+
   return (
     <div className="productList">
       <div className="container">
         <h1>Products you own</h1>
-        <div className="list-group">{loading ? <Loader /> : null}</div>
+        <div className="list-group">
+          {loading ? (
+            <Loader />
+          ) : failMessage ? (
+            <div className="text-center">No Products added yet</div>
+          ) : (
+            productList.map((product, idx) => (
+              <div
+                className="berber"
+                onClick={() => {
+                  productInfo(product[0]);
+                }}
+              >
+                <div className="berber-image">{idx + 1}.</div>
+                <p className="berber-fullname">{product[0]}</p>
+                <p className="berber-dukkan">{product[2]}</p>
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
